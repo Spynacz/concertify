52786ded857371ed8f50662f387ddbac22f4b899
Amadeusz <duzniak.amadeusz@gmail.com>
2024-03-28 22:11:46 +0100

    Add views and update urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from knox import views as knox_views
+
+from . import views
 
 app_name = 'users'
 urlpatterns = [
-    
+    path('create', views.CreateUserViews.as_view(), name='create'),
+
+    path('profile', views.ManageUserView.as_view(), name='profile'),
+
+    path('login', views.LoginView.as_view(), name='knox-login'),
+    path('logout', knox_views.LogoutView.as_view(), name='knox-logout'),
+    path('logoutall', knox_views.LogoutAllView.as_view(),
+         name='knox-logoutall')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
