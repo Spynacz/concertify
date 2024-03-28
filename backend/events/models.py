d4a6a6e996b9fdd0fc747ed5684fb338c2b02583
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-03-28 23:08:00 +0100

    Add Event and Role models

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
+class Event(models.Model):
+    title = models.CharField(_("title"), max_length=150)
+    desc = models.CharField(_("description"), max_length=300)
+    picture = models.ImageField()
+
+
+class Role(models.Model):
+    class NameChoice(models.TextChoices):
+        OWNER = ('OWNER', _('owner'))
+        MODERATOR = ('MODERATOR', _('moderator'))
+        STAFF = ('STAFF', _('staff'))
+        USER = ('USER', _('user'))
+
+    event = models.ForeignKey(Event, related_name="role",
+                              on_delete=models.CASCADE)
+    user = models.ForeignKey('users.ConcertifyUser', related_name="role",
+                             on_delete=models.CASCADE)
+    name = models.CharField(_('role name'))
+
+    class Meta:
+        unique_together = [['event', 'user']]
