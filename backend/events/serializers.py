25cd57d6698ff267bed6e2104c4de91bb70aade0
Amadeusz <duzniak.amadeusz@gmail.com>
2024-03-29 16:57:38 +0100

    Add role creation when creating event

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 
-from events import models
+from events.models import Event, Role
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
-        models = models.Event
+        model = Event
         fields = '__all__'
+
+    def create(self, validated_data):
+        event = Event.objects.create(**validated_data)
+        Role.objects.create(
+            event=event,
+            user=self.context.get('request').user,
+            name=Role.NameChoice.OWNER
+        )
+        return event
 
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Role
+        model = Role
         fields = '__all__'
---
+++ backend/events/tests/tests_serializers.py
@@ -0,0 +1,42 @@
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework.test import APIRequestFactory
+
+from knox.models import AuthToken
+
+from events.models import Role
+from events.serializers import EventSerializer
+from users.models import ConcertifyUser
+
+
+class TestEventSerializer(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = ConcertifyUser.objects.create_user(
+            username='test',
+            password='test'
+        )
+        self.data = {
+            'title': 'test',
+            'desc': 'Test test'
+        }
+        self.token = f"Token {AuthToken.objects.create(user=self.user)[-1]}"
+
+    def test_create(self):
+        """create method should make an owner role
+           for the user that creates it."""
+        request = self.factory.get(reverse('events:event-list'))
+        request.user = self.user
+
+        serializer = EventSerializer(
+            context={'request': request},
+            data=self.data
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        role = Role.objects.get(user=self.user)
+
+        self.assertIsNotNone(role)
+        self.assertEqual(role.name, Role.NameChoice.OWNER)
