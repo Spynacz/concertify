4100a17ccf557b5b0d3572483e24064d64c8623d
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-03-29 19:43:50 +0100

    Add EventContact and SocialMedia views and serializers

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -1,24 +1,36 @@
 from rest_framework import serializers
 
-from events.models import Event, Role
+from events import models
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Event
+        model = models.Event
         fields = '__all__'
 
     def create(self, validated_data):
-        event = Event.objects.create(**validated_data)
-        Role.objects.create(
+        event = models.Event.objects.create(**validated_data)
+        models.Role.objects.create(
             event=event,
             user=self.context.get('request').user,
-            name=Role.NameChoice.OWNER
+            name=models.Role.NameChoice.OWNER
         )
         return event
 
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Role
+        model = models.Role
+        fields = '__all__'
+
+
+class EventContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.EventContact
+        fields = '__all__'
+
+
+class SocialMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.SocialMedia
         fields = '__all__'
