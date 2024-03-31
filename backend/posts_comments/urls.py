c0a917411d9327340da770995a56fc8b0b5f14fd
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-03-31 20:09:08 +0200

    Add ViewSets in posts_comments app

--- backend/posts_comments/urls.py
+++ backend/posts_comments/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from posts_comments import views
 
 
 app_name = 'events'
-urlpatterns = [
+router = DefaultRouter(trailing_slash=False)
 
-]
+router.register('post', views.PostViewSet, basename='post')
+router.register('comment', views.CommentViewSet, basename='comment')
+router.register('post/vote', views.PostVoteViewSet, basename='post-vote')
+router.register('comment/vote', views.CommentVoteViewSet,
+                basename='comment-vote')
+
+
+urlpatterns = router.urls
