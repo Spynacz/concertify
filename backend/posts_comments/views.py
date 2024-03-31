aedcc03a863a2d1352b921eefdec5d96036e6339
Amadeusz <duzniak.amadeusz@gmail.com>
2024-03-31 20:09:08 +0200

    Add ViewSets in posts_comments app

--- backend/posts_comments/views.py
+++ backend/posts_comments/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from posts_comments import serializers
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.CommentSerializer
+
+
+class PostVoteViewSet(mixins.CreateModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    serializer_class = serializers.PostVoteSerializer
+    authentication_classes = [IsAuthenticated]
+
+
+class CommentVoteViewSet(mixins.CreateModelMixin,
+                         mixins.DestroyModelMixin,
+                         viewsets.GenericViewSet):
+    serializer_class = serializers.CommentVoteSerializer
+    authentication_classes = [IsAuthenticated]
