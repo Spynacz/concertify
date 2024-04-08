22302279dcd1dc8d351a3a4888fdefdfbee387ec
Amadeusz <duzniak.amadeusz@gmail.com>
2024-04-08 15:33:45 +0200

    Add Location, Ticket and ScheduleItem models

--- backend/events/models.py
+++ backend/events/models.py
@@ -4,10 +4,20 @@ from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
 
+class Location(models.Model):
+    name = models.CharField(_('location name'), max_length=150)
+    address_line = models.CharField(_('address line'), max_length=300)
+    city = models.CharField(_('city'), max_length=100)
+    postal_code = models.CharField(_('postal code'), max_length=20)
+    country = models.CharField(_('country'), max_length=3)
+
+
 class Event(models.Model):
     title = models.CharField(_("title"), max_length=150)
     desc = models.CharField(_("description"), max_length=300)
     picture = models.ImageField(blank=True, null=True)
+    location = models.ForeignKey(Location, related_name='event',
+                                 on_delete=models.DO_NOTHING)
 
 
 class Role(models.Model):
@@ -56,3 +66,21 @@ class SocialMedia(models.Model):
                 fields=['event', 'link'], name='unique_event_link_combination'
             )
         ]
+
+
+class ScheduleItem(models.Model):
+    title = models.CharField(_('title'), max_length=150)
+    desc = models.CharField(_('description'), max_length=300)
+    place = models.CharField(_('place'), max_length=150)
+    when = models.DateTimeField(_('when it will happen'))
+    event = models.ForeignKey(Event, related_name='schedule',
+                              on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    title = models.CharField(_('title'), max_length=150)
+    desc = models.CharField(_('description'), max_length=300)
+    quantity = models.IntegerField(_('quantity'))
+    amount = models.DecimalField(_('amount'), max_digits=9, decimal_places=2)
+    event = models.ForeignKey(Event, related_name='ticket',
+                              on_delete=models.CASCADE)
