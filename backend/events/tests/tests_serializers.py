99a402ac9f35866cadf0c667153f9589a22874ba
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-04-08 15:58:52 +0200

    Update tests

--- backend/events/tests/tests_serializers.py
+++ backend/events/tests/tests_serializers.py
@@ -5,7 +5,7 @@ from rest_framework.test import APIRequestFactory
 
 from knox.models import AuthToken
 
-from events.models import Role
+from events.models import Location, Role
 from events.serializers import EventSerializer
 from users.models import ConcertifyUser
 
@@ -17,9 +17,17 @@ class TestEventSerializer(TestCase):
             username='test',
             password='test'
         )
+        location = Location.objects.create(
+            name='test',
+            address_line='test',
+            city='test',
+            postal_code='test',
+            country='TST'
+        )
         self.data = {
             'title': 'test',
-            'desc': 'Test test'
+            'desc': 'Test test',
+            'location': location.id
         }
         self.token = f"Token {AuthToken.objects.create(user=self.user)[-1]}"
 
