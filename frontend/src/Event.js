4e43ec22ca9ccd3fe8038b14b86e3ebce8dc5f43
Paczur <149335135+Paczur@users.noreply.github.com>
2024-04-09 13:29:16 +0200

    Event preview tempate and move filter/sort buttons to navbar

--- frontend/src/Event.js
+++ frontend/src/Event.js
@@ -1,8 +1,18 @@
 import './Event.css'
 
-export function EventPreview() {
+export function EventPreview({ title, location, date, image }) {
   return (
     <div className="event-preview">
+      <div className="event-preview-image-container">
+        <img className="event-preview-image" src={image}/>
+      </div>
+      <div className="event-preview-data">
+        <div className="event-preview-title">{title}</div>
+        <div className="event-preview-info">
+          <div className="event-preview-location">{location}</div>
+          <div className="event-preview-date">{date}</div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -10,14 +20,17 @@ export function EventPreview() {
 export function EventList({ count }) {
   return (
     <div className="event-list-container">
-      <ul className="event-list-tools">
-        <li>Sort</li>
-        <li>Filter</li>
-        <li>Search</li>
-      </ul>
       <ul className="event-list">
         {
-          Array.from({length: count}, (_, i) => <li key={i}><EventPreview/></li>)
+          Array.from({length: count}, (_, i) => 
+            <li key={i}>
+              <EventPreview 
+                title="Kinda normal length test title"
+                image="https://weknowyourdreams.com/images/party/party-12.jpg"
+                location="Somewhere 23/15"
+                date="12/04/2024"
+              />
+            </li>)
         }
       </ul>
     </div>
