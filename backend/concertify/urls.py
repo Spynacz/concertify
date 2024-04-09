521c96f3a08391951233fea9d9678f44398046ea
Amadeusz <duzniak.amadeusz@gmail.com>
2024-04-09 17:49:43 +0200

    Fix urls app_name and linking with posts_comments app

--- backend/concertify/urls.py
+++ backend/concertify/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts_comments.urls')),
     path('', include('events.urls')),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
 ]
