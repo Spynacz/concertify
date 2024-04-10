195718f8fa3b5dd1f906c92590f07c8a8ce6e9ce
Amadeusz <duzniak.amadeusz@gmail.com>
2024-04-10 16:54:20 +0200

    Add EventDetailsSerializer tests

--- backend/events/tests/tests_serializers.py
+++ backend/events/tests/tests_serializers.py
@@ -5,13 +5,14 @@ from rest_framework.test import APIRequestFactory
 
 from knox.models import AuthToken
 
-from events.models import Location, Role
-from events.serializers import EventSerializer, LocationSerializer
+from events import serializers
+from events.models import Event, Location, Role
 from users.models import ConcertifyUser
 
 
 class TestLocationSerializer(TestCase):
     def setUp(self):
+        self.serializer_class = serializers.LocationSerializer
         self.data = {
             'name': 'test',
             'address_line': 'test',
@@ -22,7 +23,7 @@ class TestLocationSerializer(TestCase):
 
     def test_create_new(self):
         """When creating not existing object nothing should happen"""
-        serializer = LocationSerializer(data=self.data)
+        serializer = self.serializer_class(data=self.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
@@ -30,15 +31,16 @@ class TestLocationSerializer(TestCase):
         """When attempting to create existing object nothing should happen"""
         location = Location.objects.create(**self.data)
 
-        serializer = LocationSerializer(data=self.data)
+        serializer = self.serializer_class(data=self.data)
         serializer.is_valid(raise_exception=True)
         instance = serializer.save()
 
         self.assertEqual(location, instance)
 
 
-class TestEventSerializer(TestCase):
+class TestEventFeedSerializer(TestCase):
     def setUp(self):
+        self.serializer_class = serializers.EventFeedSerializer
         self.factory = APIRequestFactory()
         self.user = ConcertifyUser.objects.create_user(
             username='test',
@@ -64,7 +66,7 @@ class TestEventSerializer(TestCase):
         request = self.factory.get(reverse('events:event-list'))
         request.user = self.user
 
-        serializer = EventSerializer(
+        serializer = self.serializer_class(
             context={'request': request},
             data=self.data
         )
@@ -75,3 +77,55 @@ class TestEventSerializer(TestCase):
 
         self.assertIsNotNone(role)
         self.assertEqual(int(role.name), Role.NameChoice.OWNER)
+
+
+class TestEventDetailsSerializer(TestCase):
+    fixtures = ['fixtures/test_fixture.json']
+
+    def setUp(self):
+        self.event = Event.objects.get(id=1)
+        self.serializer = serializers.EventDetailsSerializer(
+            instance=self.event)
+
+    def test_get_event_contacts(self):
+        """Method should return list of EventContact object data"""
+        event_contacts = [
+            {
+                'id': obj.id,
+                'name': obj.name,
+                'phone': obj.phone
+            } for obj in self.event.event_contact.all()
+        ]
+        self.assertEqual(
+            event_contacts,
+            self.serializer.data['event_contacts']
+        )
+
+    def test_get_social_media(self):
+        """Method should return list of SocialMedia object data"""
+        social_media = [
+            {
+                'id': obj.id,
+                'link': obj.link,
+                'platform': obj.platform
+            } for obj in self.event.social_media.all()
+        ]
+        self.assertEqual(
+            social_media,
+            self.serializer.data['social_media']
+        )
+
+    def test_get_location(self):
+        """Method should return Location object data"""
+        location = {
+                'id': self.event.location.id,
+                'name': self.event.location.name,
+                'address_line': self.event.location.address_line,
+                'postal_code': self.event.location.postal_code,
+                'country': self.event.location.country
+            }
+
+        self.assertEqual(
+            location,
+            self.serializer.data['location']
+        )
