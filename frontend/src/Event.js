86144db0df2c26cc57749836b4e080a3449a6c6a
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-04-12 13:38:48 +0200

    Add bootstrap and react-bootstrap

--- frontend/src/Event.js
+++ frontend/src/Event.js
@@ -1,38 +1,36 @@
-import './Event.css'
+import { Card, Container } from "react-bootstrap";
+import "./Event.css";
 
 export function EventPreview({ title, location, date, image }) {
   return (
-    <div className="event-preview">
-      <div className="event-preview-image-container">
-        <img className="event-preview-image" src={image}/>
-      </div>
-      <div className="event-preview-data">
-        <div className="event-preview-title">{title}</div>
+    <Card className="event-preview">
+      <Card.Img src={image} className="event-preview-image-container" />
+      <Card.Body className="event-preview-data">
+        <Card.Title className="event-preview-title">{title}</Card.Title>
         <div className="event-preview-info">
-          <div className="event-preview-location">{location}</div>
-          <div className="event-preview-date">{date}</div>
+          <Card.Text className="event-preview-location">{location}</Card.Text>
+          <Card.Text className="event-preview-date">{date}</Card.Text>
         </div>
-      </div>
-    </div>
+      </Card.Body>
+    </Card>
   );
 }
 
 export function EventList({ count }) {
   return (
-    <div className="event-list-container">
+    <Container fluid>
       <ul className="event-list">
-        {
-          Array.from({length: count}, (_, i) => 
-            <li key={i}>
-              <EventPreview 
-                title="Kinda normal length test title"
-                image="https://weknowyourdreams.com/images/party/party-12.jpg"
-                location="Somewhere 23/15"
-                date="12/04/2024"
-              />
-            </li>)
-        }
+        {Array.from({ length: count }, (_, i) => (
+          <li key={i}>
+            <EventPreview
+              title="Kinda normal length test title"
+              image="https://weknowyourdreams.com/images/party/party-12.jpg"
+              location="Somewhere 23/15"
+              date="12/04/2024"
+            />
+          </li>
+        ))}
       </ul>
-    </div>
+    </Container>
   );
 }
