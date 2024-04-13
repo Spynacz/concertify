8c022536d30a50d48ead06eaab5a7e31b1d0408f
Amadeusz <duzniak.amadeusz@gmail.com>
2024-04-13 15:16:35 +0200

    Add PaymentInfoSerializer and update UserSerializer to account for PaymentInfo create and update

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,10 +3,19 @@ from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
-from users.models import ConcertifyUser
+from users.models import ConcertifyUser, PaymentInfo
+
+
+class PaymentInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentInfo
+        fields = '__all__'
+        extra_kwargs = {'user': {'required': False}}
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payment_info = PaymentInfoSerializer(required=False)
+
     class Meta:
         model = ConcertifyUser
         fields = [
@@ -14,13 +23,32 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'username',
             'email',
-            'password'
+            'password',
+            'payment_info'
         ]
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 9}}
+        extra_kwargs = {
+            'password': {'write_only': True, 'min_length': 9}}
 
     def create(self, validated_data):
+        payment_info = validated_data.pop('payment_info', None)
         user = ConcertifyUser.objects.create_user(**validated_data)
+
+        if payment_info:
+            payment_info.update({'user': user})
+        else:
+            payment_info = {'user': user}
+        PaymentInfo.objects.create(**payment_info)
+
         return user
+
+    def update(self, instance, validated_data):
+        payment_instance = instance.payment_info
+        payment_info = validated_data.pop('payment_info')
+        payment_info.update({'user': instance})
+
+        PaymentInfoSerializer().update(payment_instance, payment_info)
+
+        return super().update(instance, validated_data)
 
 
 class AuthSerializer(serializers.Serializer):
