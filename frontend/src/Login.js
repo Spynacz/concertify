0db71359649b87969c3be7bd02a6c62a6daf19fc
paczur <paczur@Silver>
2024-04-16 00:26:55 +0200

    Visual improvements to login page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 import "./Login.css";
 
 export function Login() {
   return (
     <div className="login-form-container">
-      <form className="login-form">
-        <span>Concertify</span>
-        <input type="text" placeholder="login"/>
-        <input type="password" placeholder="password"/>
-        <input type="submit" value="Login"/>
-      </form>
+      <Form className="login-form">
+        <span className="accent-button">Concertify</span>
+        <Form.Control type="text" placeholder="login"/>
+        <Form.Control type="password" placeholder="password"/>
+        <Button className="accent-button" type="submit">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
@@ -17,14 +20,20 @@ export function Login() {
 export function Register() {
   return (
     <div className="login-form-container">
-      <form className="login-form">
-        <span>Concertify</span>
-        <input type="text" placeholder="login"/>
-        <input type="email" placeholder="email"/>
-        <input type="password" placeholder="password"/>
-        <input type="password" placeholder="password"/>
-        <input type="submit" value="Login"/>
-      </form>
+      <Form className="login-form">
+        <span className="accent-button">Concertify</span>
+        <Form.Control type="text" placeholder="username"/>
+        <Form.Control type="email" placeholder="email"/>
+        <Form.Control type="password" placeholder="password"/>
+        <Form.Control type="password" placeholder="password"/>
+        <div className="personal-info-container">
+          <Form.Control type="text" placeholder="first name"/>
+          <Form.Control type="text" placeholder="last name"/>
+        </div>
+        <Button className="accent-button" type="submit">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
