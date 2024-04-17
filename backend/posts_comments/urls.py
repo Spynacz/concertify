ae8717a583feaecad9bb71629921a83aff59ca46
Amadeusz <duzniak.amadeusz@gmail.com>
2024-04-17 13:54:35 +0200

    Update permission classes and update urls in posts_comments

--- backend/posts_comments/urls.py
+++ backend/posts_comments/urls.py
@@ -8,8 +8,8 @@ router = DefaultRouter(trailing_slash=False)
 
 router.register('post', views.PostViewSet, basename='post')
 router.register('comment', views.CommentViewSet, basename='comment')
-router.register('post/vote', views.PostVoteViewSet, basename='post-vote')
-router.register('comment/vote', views.CommentVoteViewSet,
+router.register('post-vote', views.PostVoteViewSet, basename='post-vote')
+router.register('comment-vote', views.CommentVoteViewSet,
                 basename='comment-vote')
 
 
