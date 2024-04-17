48132430e78204cb7c491c6050eccd124c0ef667
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-04-17 13:54:35 +0200

    Update permission classes and update urls in posts_comments

--- backend/posts_comments/views.py
+++ backend/posts_comments/views.py
@@ -1,26 +1,37 @@
 from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-from posts_comments import serializers
+from events.permissions import IsEventModerator
+
+from posts_comments import models, serializers
 from posts_comments.permissions import IsOwner
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.PostSerializer
+    permission_classes = [IsEventModerator]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CommentSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class PostVoteViewSet(mixins.CreateModelMixin,
                       mixins.DestroyModelMixin,
                       viewsets.GenericViewSet):
     serializer_class = serializers.PostVoteSerializer
-    authentication_classes = [IsOwner]
+    permission_classes = [IsOwner]
+
+    def get_queryset(self):
+        return models.PostVote.objects.all()
 
 
 class CommentVoteViewSet(mixins.CreateModelMixin,
                          mixins.DestroyModelMixin,
                          viewsets.GenericViewSet):
     serializer_class = serializers.CommentVoteSerializer
-    authentication_classes = [IsOwner]
+    permission_classes = [IsOwner]
+
+    def get_queryset(self):
+        return models.CommentVote.objects.all()
