010bff19a3b6f7f92d4cbb1598148f73bd284f40
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-04-19 12:12:32 +0200

    Formatting

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -4,12 +4,14 @@ const common = require("./webpack.common.js");
 const webpack = require("webpack");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   plugins: [
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-  ]
+  ],
 });
