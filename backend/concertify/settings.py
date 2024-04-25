104e41a0800395577f2c900c5d58db1791945a45
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-04-25 19:09:22 +0200

    Fix docker-compose postgres setup

--- backend/concertify/settings.py
+++ backend/concertify/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'concertify.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_NAME'),
+        'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'database',
