8957d76372eff721a4d33d0b41ac3a80453bdae5
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-04-26 15:51:29 +0200

    Restyle join button

--- frontend/src/event/JoinButton.js
+++ frontend/src/event/JoinButton.js
@@ -2,8 +2,8 @@ import { Button, Row } from "react-bootstrap";
 
 export default function JoinButton() {
   return (
-    <Row className="justify-content-xl-center mx-5">
-      <Button>Sign me up</Button>
+    <Row className="justify-content-center mx-5">
+      <Button className="w-25">Sign me up</Button>
     </Row>
   );
 }
