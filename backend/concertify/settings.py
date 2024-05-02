ea6e6391a89632aeaad37c1ba3a04b0891d6adb9
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-05-02 14:26:18 +0200

    Combine celery with cache redis

--- backend/concertify/settings.py
+++ backend/concertify/settings.py
@@ -183,10 +183,9 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
-        "KEY_PREFIX": "example"
     }
 }
 
 # Celery Configuration Options
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
