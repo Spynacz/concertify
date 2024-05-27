3d4edc9426205293071d5cd04d58e786eab28ab3
Paczur <149335135+Paczur@users.noreply.github.com>
2024-05-27 10:59:44 +0000

    Delete frontend/src/event/NewEvent.js

--- frontend/src/event/NewEvent.js
+++
@@ -1,9 +0,0 @@
-import { Form, Button } from "react-bootstrap";
-
-export default function NewEvent() {
-  return (
-    <Form>
-      <Form.Control type="text" name="title" />
-    </Form>
-  );
-}
