b7c8b4f609a33fa32772bb806dc3e71d6916b3b0
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-05-29 20:34:27 +0200

    Remove amount field from OrderItem and connect payments with urls

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -14,7 +14,6 @@ class OrderItem(models.Model):
 
     ticket_type = models.CharField(choices=TicketTypeChoice.choices)
     quantity = models.IntegerField()
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
     order = models.ForeignKey(Order, related_name='order_items',
                               on_delete=models.CASCADE)
     ticket = models.ForeignKey('events.Ticket', related_name='order_items',
--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 
-app_name = 'events'
+app_name = 'payments'
 urlpatterns = [
     path('cart', views.ShoppingCartView.as_view(), name='cart'),
 ]
