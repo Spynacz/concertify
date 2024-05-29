6fa6ba2ed8c94ce8c5b4dee1233fcb4bad3588ca
YaBoyya <110597692+YaBoyya@users.noreply.github.com>
2024-05-29 20:41:02 +0200

    Add CheckoutView

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'payments'
 urlpatterns = [
     path('cart', views.ShoppingCartView.as_view(), name='cart'),
+    path('checkout', views.CheckoutView.as_view(), name='checkout'),
 ]
