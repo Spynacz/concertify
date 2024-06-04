c9fcc2fe8c5c5d67f857d999aa76fedf643e3a58
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-06-04 22:20:11 +0200

    Add image upload to S3 Bucket

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,5 +1,7 @@
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+require("dotenv").config({ path: "./.env" });
 
 module.exports = {
   entry: "./src/index.js",
@@ -25,6 +27,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/template.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
     }),
   ],
 };
