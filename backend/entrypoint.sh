afe2a639d0d88705639e23d31d3c56dd9b0933a7
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-06-05 09:30:09 +0200

    Stop Celery whining about root access

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 python manage.py migrate --no-input
-celery -A concertify worker &
-python manage.py runserver 0.0.0.0:8000 
+celery -A concertify worker --uid=nobody --gid=nogroup &
+python manage.py runserver 0.0.0.0:8000
